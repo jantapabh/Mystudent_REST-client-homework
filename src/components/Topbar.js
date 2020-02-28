f40a88156232eb4c37bc174f34aa256f65fe8730
jantapabh <s6035512034@phuket.psu.ac.th>
2020-02-28 20:48:46 +0700

    commit components

---
+++ src/components/Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../components/Bar.css';
+
+
+const Topbar = () => {
+
+
+    return (
+        <div>
+            <div className="Bar">
+                Barrrr
+            </div>
+        </div>
+    );
+}
