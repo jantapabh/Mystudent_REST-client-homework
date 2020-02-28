6f5cbeb317ece0276c072efab8fed47fe38aff2c
jantapabh <s6035512034@phuket.psu.ac.th>
2020-02-28 20:53:18 +0700

    commit add Topbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Students from '../src/components/Student'
+import Topbar from './components/Topbar';
 
 
 const App = () => {
   return(
+<div className="container">
 
+  <Topbar />
     <div>
       Hello Worlds
       <Students />
+      </div>
       </div>
   );
 }
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -13,3 +13,5 @@ const Topbar = () => {
         </div>
     );
 }
+
+export default Topbar;
