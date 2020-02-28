8d2a60104b565c2bcbd0d190556212e3d848e5d6
jantapabh <s6035512034@phuket.psu.ac.th>
2020-02-28 20:31:55 +0700

    commit first

---
+++ src/components/Student.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import App from './App.js'
+
+
+
+const App = () => {
+
+    return(
+
+       <div>
+
+           Students
+          
+          </div>
+    );
+}
+
+export default App;
