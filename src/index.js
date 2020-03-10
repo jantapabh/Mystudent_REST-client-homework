8ca1b53e9ba266e3d8ba56de94ce38596b113402
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-10 21:20:41 +0700

    add edit

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 
 //Redux 
 
-import { createStore } from 'redux';
-import redux from 'react';
+// import { createStore } from 'redux';
+// import redux from 'react';
 
-const reducer = (state, action) => {
+// const reducer = (state, action) => {
 
-    switch (action.type) {
+//     switch (action.type) {
 
-        case "ADD":
+//         case "ADD":
 
-        state+= action.payload;
+//         state+= action.payload;
 
-            break;
+//             break;
 
-        case "SUBTRACT":
+//         case "SUBTRACT":
 
-            state-= action.payload;
+//             state-= action.payload;
 
-            break;
+//             break;
 
-        default:
-    }
+//         default:
+//     }
 
-    return state;
-}
+//     return state;
+// }
 
-const store = createStore(reducer, 1);
+// const store = createStore(reducer, 1);
 
-//dispatch จะเป็นการตรวจสอบ action
+// //dispatch จะเป็นการตรวจสอบ action
 
-// คำสั่ง subscribe เป็นการอัพเดพค่าให้กับ state
+// // คำสั่ง subscribe เป็นการอัพเดพค่าให้กับ state
 
-store.subscribe( () => {
+// store.subscribe( () => {
 
-    console.log("Update State", store.getState());
+//     console.log("Update State", store.getState());
 
-})
-store.dispatch({
+// })
+// store.dispatch({
 
-    type: "ADD",
-    payload: 500
+//     type: "ADD",
+//     payload: 500
 
-})
+// })
